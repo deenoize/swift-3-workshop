a18749fed6248ccee7c9356faca09445379df4bf
Russel Lvov <deenoize@gmail.com>
2017-04-09 16:48:13 -0700

    Coding chalenges still need to do

---
+++ CFS3ToDoList/TodoList.swift
@@ -0,0 +1,40 @@
+//
+//  TodoList.swift
+//  CFS3ToDoList
+//
+//  Created by Ruslan Galbatsdibirov on 09/04/2017.
+//  Copyright © 2017 Adam Wallraff. All rights reserved.
+//
+
+import Foundation
+
+class TodoList {
+    static let shared = TodoList()
+    
+    var allTodos = [Todo]()
+    
+    private init(){}
+    
+    func add (todo: Todo) {
+        self.allTodos.append(todo)
+    }
+    
+    func remove (todo: Todo) {
+        self.allTodos = self.allTodos.filter({ (item) -> Bool in
+            return item.identifier != todo.identifier
+        })
+    }
+    
+    func removeAll() {
+        self.allTodos.removeAll()
+    }
+    
+    func getTodoAt (index: Int) -> Todo {
+        return self.allTodos[index]
+    }
+    
+    func count() -> Int {
+        return self.allTodos.count
+    }
+    
+}
